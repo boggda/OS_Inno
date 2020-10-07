f610c2d06fbb85f2f70f0aa0f81741ca94b5f78c
boggda <avgust45@gmail.com>
2020-10-07 13:08:36 +0000

    week8 added

--- week8/ex4.c
+++ week8/ex4.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/resource.h>
 
 int main()
 {
   void *ptr; // pointer to memmory
+  struct rusage usage; // struct declaration
   int size = 100 * 1024 * 1024; // 100 MB
   for(int i = 0; i < 10; i++)
   {
     ptr = malloc(size); // allocate 100 MB
     memset(ptr, '\0', size); // fill allocated memmory with zeros
+    if (getrusage(RUSAGE_SELF, &usage)) // get usage of current proccess.
+      printf("[!] Some error.\n");
+
+    printf("[+] Mem usage: %ld\n", usage.ru_maxrss); // print mem usage
     sleep(1); // wait 1 second
   }
   printf("[+] Done\n");
   return 0;
 }
-
-/*
- * When there is not enough physical memory, some pages are swapped.
- */
